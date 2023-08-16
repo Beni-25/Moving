dd8abad13c2dcf47ef2b5626ba2640bd242a0088
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-08-16 20:33:58 -0230

    Add & Remove Class

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -83,12 +83,45 @@ $( "#bookingbtn" ).on( 'mouseenter', () => {
     $( "#bookingbtn" ).css({
         color: 'white',
         // backgroundColor:'#e6a481'
-    })
+    });
+    $( "#bookingbtn" ).animate({
+        fontSize: '20px'
+            },200);
   } );
 
   $( "#bookingbtn" ).on( 'mouseleave', () => {
     $( "#bookingbtn" ).css({
         color: 'black', 
         // backgroundColor:'#e4966b'
-    })
+    });
+    $( "#bookingbtn" ).animate({
+        fontSize: '16px'
+            },200);
   } );
+
+$('.bi-facebook').on('mouseenter', ()=>{
+    $('.bi-facebook').addClass('bi-active');
+})
+$('.bi-facebook').on('mouseleave', ()=>{
+    $('.bi-facebook').removeClass('bi-active');
+})
+
+$('.bi-envelope').on('mouseenter', ()=>{
+    $('.bi-envelope').addClass('bi-active');
+})
+$('.bi-envelope').on('mouseleave', ()=>{
+    $('.bi-envelope').removeClass('bi-active');
+})
+
+$('.bi-linkedin').on('mouseenter', ()=>{
+    $('.bi-linkedin').addClass('bi-active');
+})
+$('.bi-linkedin').on('mouseleave', ()=>{
+    $('.bi-linkedin').removeClass('bi-active');
+})
+$('.bi-instagram').on('mouseenter', ()=>{
+    $('.bi-instagram').addClass('bi-active');
+})
+$('.bi-instagram').on('mouseleave', ()=>{
+    $('.bi-instagram').removeClass('bi-active');
+})
