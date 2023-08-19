7176d41e1cd0aac0a74a11bdd25fd412f2ea1293
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-08-19 21:21:31 -0230

    Form Testing

---
+++ tests/bookingFormWithTopButtons/bookingform.js
@@ -0,0 +1,12 @@
+function showStep(stepNumber) {
+    const formSteps = $('.form-step');
+     const stepTabs = $('.step-tab');
+
+       stepTabs.removeClass('active');
+     formSteps.removeClass('active');
+     
+          stepTabs.eq(stepNumber - 1).addClass('active');
+     
+        formSteps.eq(stepNumber - 1).addClass('active');
+   }
+   
