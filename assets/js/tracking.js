ce8841c1033ece42e9e4a356b633715e2142805c
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-08-31 22:38:58 -0230

    Tracking Page

---
+++ assets/js/tracking.js
@@ -0,0 +1,29 @@
+// Functions
+
+$(".bi-facebook").on("mouseenter", () => {
+    $(".bi-facebook").addClass("bi-active");
+  });
+  $(".bi-facebook").on("mouseleave", () => {
+    $(".bi-facebook").removeClass("bi-active");
+  });
+  
+  $(".bi-envelope").on("mouseenter", () => {
+    $(".bi-envelope").addClass("bi-active");
+  });
+  $(".bi-envelope").on("mouseleave", () => {
+    $(".bi-envelope").removeClass("bi-active");
+  });
+  
+  $(".bi-linkedin").on("mouseenter", () => {
+    $(".bi-linkedin").addClass("bi-active");
+  });
+  $(".bi-linkedin").on("mouseleave", () => {
+    $(".bi-linkedin").removeClass("bi-active");
+  });
+  $(".bi-instagram").on("mouseenter", () => {
+    $(".bi-instagram").addClass("bi-active");
+  });
+  $(".bi-instagram").on("mouseleave", () => {
+    $(".bi-instagram").removeClass("bi-active");
+  });
+  
