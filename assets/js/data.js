fa6d9b1172078a403d51de0899aff3d0a61696bf
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-09-11 21:33:32 -0230

    Index Page dynamic cards

--- assets/js/data.js
+++ assets/js/data.js
@@ -21,3 +21,24 @@ var summaryList = [
   textline3:"DO YOU HAVE ANY STAIRS/STEPS IN THE LOCATION? NO", textline4:"INSTRUCTIONS: LEAVE AT THE DOORSTEP" },
 
 ];
+
+var carouselList=[
+
+{imageclass:"active carousel-image1-wrapper"},
+{imageclass:"carousel-image2-wrapper"},
+{imageclass:"carousel-image3-wrapper"},
+{imageclass:"carousel-image4-wrapper"},
+
+];
+
+var cardList=[
+{image:"img1.jpeg", imagename:"Furniture moving image", title:"Reliable", cardtext:"From studio apartments all the way up to multi " + 
+"bedroom homes, you can trust us with your prized possessions with safe packing and unpacking."},
+{image:"img2.jpeg", imagename:"Mattress moving image", title:"Loading/Unloading", cardtext:"Choose full-residential moving or small " + 
+"furniture moving options. Our highly trained crews ensure your belongings arrive safe and sound."},
+{image:"img3.jpeg", imagename:"Couch moving image", title:"Professional", cardtext:" We offer a professionally trained and background " + 
+"checked moving team to ensure your belongings are handled with care."},
+{image:"img4.jpeg", imagename:"Mattress moving in Truck image", title:"Furniture Moving", cardtext:"Our professional furniture movers " + 
+"and furniture delivery services will transport your items safely and economically."},
+
+];
