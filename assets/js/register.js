6b36692a107dc6fe37397216f35611a83cdac014
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-09-19 22:27:14 -0230

    register page

--- assets/js/register.js
+++ assets/js/register.js
@@ -2,14 +2,14 @@
 $(document).ready(function () {
   // On register Page redirect to home page
   $("button.signup").on("click", function (e) {
-    username = $('input#username').val();
-    password =  $('input#password').val();
-    c_password =  $('input#c_password').val();
-    phone =  $('input#phone').val();
-    email=  $('input#email').val();
+    const username = $('input#username').val();
+    const password =  $('input#password').val();
+    const c_password =  $('input#c_password').val();
+    const phone =  $('input#phone').val();
+    const email=  $('input#email').val();
     let user = saveUser(username, password, c_password, phone, email);
     if(user == null){
-        alert("Error creating user")
+        alert("Error creating user");
     }else{
         console.log("Signing up", e);
         e.preventDefault(); // Stops <form> from reloading the page
