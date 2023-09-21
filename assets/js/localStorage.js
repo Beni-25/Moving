395f566b95112ca3130c1208decfb211b5977a7b
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-09-21 18:21:27 -0230

    Update localStorage.js

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -11,17 +11,17 @@ function getAllUsers() {
 }
 
 function getUser(username) {
-    console.log("Get User");
-    let allUsers = localStorage.getItem(window.USER_LIST_KEY);
+    console.log("Get User"); 
+    let allUsers = localStorage.getItem(window.USER_LIST_KEY); // [ {username: "1", password: "2"..}, {username: "3", password: "4"..} ]
     if (allUsers == null || allUsers == "" ) {
         return null;
     } else {
-        allUsers =JSON.parse(allUsers);
+        allUsers =JSON.parse(allUsers);//
         console.log("Listing all users", allUsers);
-        allUsers.forEach(user => {
-            if(user["username"] == username) {
+        allUsers.forEach(user => {  // {username: "1", password: "2"..}
+            if(user["username"] == username) { // user["username"] = 1
                 console.log("User found", user)
-                return user;
+                return user; // {username: "1", password: "2"..}
             }
         });
         console.log("User not found")
@@ -30,23 +30,25 @@ function getUser(username) {
   }
 
 function saveUser(username, password, c_password, phone, email) {
-  console.log("Saving user");
-  if (password !== c_password) {
-    alert("Password is not matching with confirm password");
-    return;
-  } else {
-    let allUsers = getAllUsers();
-    console.log("Getting all users", allUsers)
-    let user = getUser(username);
+  console.log("Saving user"); 
+  if (password !== c_password) { // password compare with c_ password
+    alert("Password is not matching with confirm password"); //alert shows not matching
+    return null; // password not equal c_ password; return to register.js saveUser function
+  } else { //Password is same as c_password
+    let allUsers = getAllUsers();// call getAllUsers() function, saves data into allUsers, e.g., [ {username: "1", password: "2"..}, {username: "3", password: "4"..} ]
+    console.log("Getting all users", allUsers) 
+    let user = getUser(username); // 
+    console.log(user, "saveUser | getUser(username)");
     if (user == null) {
-      const USER_KEY = username;
       const USER_PAYLOAD = {
         username: username,
         password: password,
         phone: phone,
         email: email,
       };
-      localStorage.setItem(USER_KEY,JSON.stringify(USER_PAYLOAD));
+      allUsers.push()
+      //localStorage.setItem(USER_KEY,JSON.stringify(USER_PAYLOAD));
+      localStorage.setItem(window.USER_LIST_KEY ,JSON.stringify(allUsers));
       console.log("User payload", USER_PAYLOAD);
       return getUser(username); 
     } else {
@@ -56,3 +58,5 @@ function saveUser(username, password, c_password, phone, email) {
   }
 
 }
+
+
