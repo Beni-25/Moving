b95548863e583c04cccba532a520af891e4de25f
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-09-23 23:12:09 -0230

    lines explanation

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -1,9 +1,9 @@
 function getAllUsers() {
     console.log("Inside getALLUSERS");
-  let allUsers = localStorage.getItem(window.USER_LIST_KEY);// get the values from window.USER_LIST_KEY  ie,USERS
+  let allUsers = localStorage.getItem(window.USER_LIST_KEY);// get the values from window.USER_LIST_KEY  
   if (allUsers == null || allUsers == "" ) {// if allUsers is null or empty
     console.log("Null in ALLUSERS");
-    localStorage.setItem(window.USER_LIST_KEY, JSON.stringify([]));// set [] as string (ie,[] to "[]") empty array to string
+    localStorage.setItem(window.USER_LIST_KEY, JSON.stringify([]));// set USERS [] as string (ie,[] to "[]") empty array to string
     return [];//return empty array to saveUser function
   } else {//if allUsers has any values
     return JSON.parse(allUsers);//return  converted string to Javascript object to saveUser function eg: '{username: "1", password: "2"..}' to {username: "1", password: "2"..}
@@ -12,14 +12,14 @@ function getAllUsers() {
 
 function getUser(username) {
     console.log("Get User"); 
-    let allUsers = localStorage.getItem(window.USER_LIST_KEY); // [ {username: "1", password: "2"..}, {username: "3", password: "4"..} ]
+    let allUsers = localStorage.getItem(window.USER_LIST_KEY); // [ {username: "1", password: "2"..}, {username: "3", password: "4"..} ] or []
     if (allUsers == null || allUsers == "" ) {//if allUsers is null or empty
         return null;//return null to saveUser function
     } else {//if allUsers has any values
-        allUsers =JSON.parse(allUsers);//get the Javascript object from string in allUsers ie,'{username: "1", password: "2"..}' to {username: "1", password: "2"..}
+        allUsers =JSON.parse(allUsers);//get the Javascript object from string in allUsers ie,'[{username: "1", password: "2"..}]' to {username: "1", password: "2"..}
         console.log("Listing all users", allUsers);
         allUsers.forEach(user => {  // {username: "1", password: "2"..} (each set of object will be called one by one using forEach)user means a set of object; allUsers means array of user objects
-            if(user["username"] == username) { // user["username"] = 1; checks whether current set user object username property matches the username
+            if(user["username"] == username) { // user["username"] = 1; checks whether current set user object username property matches the username given
                 console.log("User found", user)
                 return user; // {username: "1", password: "2"..} returns user object (exit current set of object)
             }
@@ -31,29 +31,29 @@ function getUser(username) {
 
 function saveUser(username, password, c_password, phone, email) {//saveUser(username:"1", password:"2", c_password:"2", phone:"3", email:"4")
   console.log("Saving user"); 
-  if (password !== c_password) { // password compare with c_ password
+  if (password !== c_password) { // password compare with c_ password whether not equal
     alert("Password is not matching with confirm password"); //alert shows if it is not matching
     return null; // password not equal c_ password; return to register.js saveUser function
   } else { //Password is same as c_password
     let allUsers = getAllUsers();// call getAllUsers() function, saves data into allUsers, e.g., [ {username: "1", password: "2"..}, {username: "3", password: "4"..} ] or []
     console.log("Getting all users", allUsers) 
-    let user = getUser(username); // call getUser() function, saves user as either a set of user object or null
+    let user = getUser(username); // call getUser() function, saves user as either a set of user object or null; if username matching then gives user object ie, {username: "1", password: "2"..}
     console.log(user, "saveUser | getUser(username)");
     if (user == null) {// if user is null, execute below code
       const USER_PAYLOAD = {
-        username: username, //get values as object in USER_PAYLOAD ie, { username:"1", password:"2", c_password:"2", phone:"3", email:"4"}
+        username: username, //get values as object in USER_PAYLOAD ie, { username:"1", password:"2", phone:"3", email:"4"}
         password: password,
         phone: phone,
         email: email,
       };
-      allUsers.push(USER_PAYLOAD);
+      allUsers.push(USER_PAYLOAD);// push the object ie, { username:"1", password:"2", phone:"3", email:"4"} to allUsers array
       //localStorage.setItem(USER_KEY,JSON.stringify(USER_PAYLOAD));
-      localStorage.setItem(window.USER_LIST_KEY ,JSON.stringify(allUsers));
+      localStorage.setItem(window.USER_LIST_KEY ,JSON.stringify(allUsers)); //set the object value as string ie, "[{ username:"1", password:"2", phone:"3", email:"4"}]"" 
       console.log("User payload", USER_PAYLOAD);
-      return getUser(username); 
-    } else {
+      return getUser(username); // go to getUser(username) function to check the username exists or not
+    } else { // if user has value and same username exists
       alert(`User with username ${username} already exists.`);// alert to indicate the same username exists
-      return null;// 
+      return null;// return to inside if loop to create new USER_PAYLOAD
     }
   }
 
