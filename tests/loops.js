e2c66186784066aebdf5f12677654acb159bf957
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-09-24 22:48:49 -0230

    testing

--- tests/loops.js
+++ tests/loops.js
@@ -1,10 +1,15 @@
+var testLoops =[
+    {name:"NAME: BENI"},
+    {name:"NAME: BENIS"},
+    {name:"NAME: BENISHA"}
 
+];
 
 function renderCards() {
-for(i=0;i<4;i++){
-   const loops= testLoops[i];
+for(i=0;i<3;i++){
+  const loops = testLoops[i];
 
-$(".cards").append(`<div class="card mb-3 container-contact lpcard" style="max-width: 540px;">
+$(".card").append(`<div class="card mb-3 container-contact lpcard" style="max-width: 540px;">
 <div class="row g-0">
   <div class="col-md-4">
     <img src="/assets/css/images/profileuser.png" class="img-fluid rounded-start" alt="User icon">
@@ -12,7 +17,7 @@ $(".cards").append(`<div class="card mb-3 container-contact lpcard" style="max-w
   <div class="col-md-8">
     <div class="card-body">
       <h5 class="card-title">Contact Details</h5>
-      <p class="card-text">NAME: BENISHA</p>
+      <p class="card-text">${loops[name]}</p>
       <p class="card-text">PHONE NUMBER: +1 709 351 6971</p>
       <p class="card-text">EMAIL ADDRESS: benisha@gmail.com</p>
     </div>
@@ -21,3 +26,7 @@ $(".cards").append(`<div class="card mb-3 container-contact lpcard" style="max-w
 </div>`);
 }
 }
+
+$(document).ready(function () {
+    renderCards();
+});
