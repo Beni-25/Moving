8df72d407fab6807ccb5af541253e6d3b3376d05
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-09-26 23:06:57 -0230

    while loop

--- tests/loops.js
+++ tests/loops.js
@@ -28,6 +28,32 @@ $(".cardsrepetition").append(`<div class="card mb-3 container-contact lpcard" st
 }
 }
 
+function renderWhileCards() {
+  let i=0;
+  while(i<testLoops.length){
+    let loops = testLoops[i];
+    console.log(testLoops.length);
+  
+  $(".cardsrepetition").append(`<div class="card mb-3 container-contact lpcard" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="/assets/css/images/profileuser.png" class="img-fluid rounded-start" alt="User icon">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Contact Details Using While</h5>
+        <p class="card-text">${loops["Name"]}</p>
+        <p class="card-text">PHONE NUMBER: +1 709 351 6971</p>
+        <p class="card-text">EMAIL ADDRESS: benisha@gmail.com</p>
+      </div>
+    </div>
+  </div>
+  </div>`);
+  i++;
+  }
+  }
+
 $(document).ready(function () {
     renderCards();
+    renderWhileCards();
 });
