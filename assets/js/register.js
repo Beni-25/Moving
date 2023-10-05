af4ae2590c552c4a0365c2d3931637413aee1414
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-10-05 21:12:22 -0230

    Login & Register Completed

--- assets/js/register.js
+++ assets/js/register.js
@@ -2,7 +2,6 @@
 $(document).ready(function () {
   loggedInUser = getLoggedInUser();
 
-  
   if (loggedInUser == null) {
     // On register Page redirect to home page
     $("button.signup").on("click", function (e) {
@@ -11,6 +10,18 @@ $(document).ready(function () {
       const c_password = $("input#c_password").val();
       const phone = $("input#phone").val();
       const email = $("input#email").val();
+      // Check if inputs not empty
+      if (
+        username.trim() === "" ||
+        password.trim() === "" ||
+        c_password.trim() === "" ||
+        phone.trim() === "" ||
+        email.trim() === ""
+      ) {
+        alert("Please enter all the details.");
+        return; // Prevent further execution
+      }
+
       let user = saveUser(username, password, c_password, phone, email); //eg:username:"1", password:"2", c_password:"2", phone:"3", email:"4"
       if (user == null) {
         alert("Error creating user");
@@ -21,6 +32,6 @@ $(document).ready(function () {
       }
     });
   } else {
-    // window.location.href = "index.html";
+    window.location.href = "index.html";
   }
 });
