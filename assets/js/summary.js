b59b9adad6243644669a5fe7e80bd76ca0f65ee0
Beni-25 <105207468+Beni-25@users.noreply.github.com>
2023-11-05 23:39:01 -0330

    Update

--- assets/js/summary.js
+++ assets/js/summary.js
@@ -116,46 +116,6 @@ $(document).ready(function () {
     }
   );
 
-  //social media buttons color change on mouse enter and leave
-  $(".bi-facebook").on("mouseenter", () => {
-    $(".bi-facebook").addClass("bi-active");
-  });
-  $(".bi-facebook").on("mouseleave", () => {
-    $(".bi-facebook").removeClass("bi-active");
-  });
-
-  $(".bi-envelope").on("mouseenter", () => {
-    $(".bi-envelope").addClass("bi-active");
-  });
-  $(".bi-envelope").on("mouseleave", () => {
-    $(".bi-envelope").removeClass("bi-active");
-  });
-
-  $(".bi-linkedin").on("mouseenter", () => {
-    $(".bi-linkedin").addClass("bi-active");
-  });
-  $(".bi-linkedin").on("mouseleave", () => {
-    $(".bi-linkedin").removeClass("bi-active");
-  });
-  $(".bi-instagram").on("mouseenter", () => {
-    $(".bi-instagram").addClass("bi-active");
-  });
-  $(".bi-instagram").on("mouseleave", () => {
-    $(".bi-instagram").removeClass("bi-active");
-  });
-
-  //Booking Button color change on mouseenter & mouseleave
-  $("#bookingbtn").on("mouseenter", () => {
-    $("#bookingbtn").css({
-      color: "white",
-    });
-  });
-
-  $("#bookingbtn").on("mouseleave", () => {
-    $("#bookingbtn").css({
-      color: "black",
-    });
-  });
   let loggedInUser = getLoggedInUser();
   if (loggedInUser !== null) {
     $("#name").text("NAME: " + loggedInUser["username"]);
